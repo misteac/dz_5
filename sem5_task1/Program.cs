b44e610c941ed70eeb389000d9601870c50945fe
arew <misteac13@gmail.com>
2023-11-23 09:40:13 +0300

    ДЗ к семенирау 5 задача 1

---
+++ sem5_task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int find_elenent(int i , int j)
+{
+    int[,] array2 = new int[,] {{1, 2, 3}, 
+                                {4, 5, 6},
+                                {7, 8, 9}};
+ if (i < 4 && j < 4)
+return array2[i,j];
+ else 
+ Console.WriteLine("Такого элемента не существует");
+ return 0;
+}
+Console.WriteLine("Введиое номер строки элемента в двумерном массиве ");
+int i = Convert.ToInt32(Console.ReadLine()) -1;
+Console.WriteLine("Введиое номер столбца элемента в двумерном массиве ");
+int j = Convert.ToInt32(Console.ReadLine()) -1;
+int result = find_elenent(i,j);
+Console.WriteLine(result);
