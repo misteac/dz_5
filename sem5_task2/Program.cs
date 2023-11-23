e6c751dbb5d269197551d854a1912289a9e191a3
arew <misteac13@gmail.com>
2023-11-23 10:14:53 +0300

    Задача 2

--- sem5_task2/Program.cs
+++ sem5_task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+            var array = new int[4, 4]
+            { { 1, 2, 3, 4 },
+              { 0, 0, 0, 0 },
+              { 0, 0, 0, 0 },
+              { 5, 6, 7, 8 }
+            };
+            for(int i = 0; i < array.GetLength(1); i++)
+            {
+                var temp = array[3, i];
+                array[3, i] = array[0, i];
+                array[0, i] = temp;
+            }
+            for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
