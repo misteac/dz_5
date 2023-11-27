a52cb32e62ddf005078ffafdd21192c36404f48b
arew <misteac13@gmail.com>
2023-11-27 14:51:46 +0300

    Задача 1 Домашнее задание семенар 6

---
+++ sem6_task1_dz/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[] array = {'a', 'b', 'c', 'd'};
+
+string ArrayString (char[] array)
+{
+    string result = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = array[i] + result; // перевернули обратно,иначе: result +c array[i]
+    }
+    return result;
+}
+Console.WriteLine(ArrayString(array));
