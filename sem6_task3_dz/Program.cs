debb1d3f35218fdb0efe2530f8225eb1f431642a
arew <misteac13@gmail.com>
2023-11-27 14:50:55 +0300

    Задача 3 Домашнее задание семенар 6

---
+++ sem6_task3_dz/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+
+Console.WriteLine("Введите произвольную строку");
+string text = Console.ReadLine().ToLower();
+
+string palindrom( string message)
+{   
+    string check = string.Empty;
+    for( int i = 0; i < text.Length; i++)
+    {
+        check = message[i] + check;
+    }
+    if ( check == message)
+    {
+        Console.WriteLine($"{text} строка явялется палиндромом");
+    }
+    else 
+    {
+        Console.WriteLine($"{text} строка не явялется палиндромом");
+    }
+    return check;
+}
+palindrom(text);
