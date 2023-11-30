f66a26fe3eb48265a2ab54d81ce042bee27641c5
arew <misteac13@gmail.com>
2023-11-30 15:17:04 +0300

    Задача 2 Семенар 7 ДЗ

---
+++ sem7_task2_dz/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+akkerman_function(m,n);
+
+
+void akkerman_function(int m, int n)
+{
+    Console.Write(akkerman(m, n)); 
+}
+
+
+int akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (akkerman(m - 1, akkerman(m, n - 1)));
+    }
+}
